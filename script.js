6fca7cf6590fb6732f36f8e00ec35632101ae110
GiuliaPicasso <giulia.picasso@mail.polimi.it>
2022-06-06 22:14:07 +0200

    6/06

--- script.js
+++ script.js
@@ -1,19 +1,11 @@
-(function() {
-    // Add event listener
-    document.addEventListener("mousemove", parallax);
-    const elem = document.querySelector("#parallax");
-    // Magic happens here
-    function parallax(e) {
-        let _w = window.innerWidth/2;
-        let _h = window.innerHeight/2;
-        let _mouseX = e.clientX;
-        let _mouseY = e.clientY;
-        let _depth1 = `${50 - (_mouseX - _w) * 0.01}% ${50 - (_mouseY - _h) * 0.01}%`;
-        let _depth2 = `${50 - (_mouseX - _w) * 0.02}% ${50 - (_mouseY - _h) * 0.02}%`;
-        let _depth3 = `${50 - (_mouseX - _w) * 0.06}% ${50 - (_mouseY - _h) * 0.06}%`;
-        let x = `${_depth3}, ${_depth2}, ${_depth1}`;
-        console.log(x);
-        elem.style.backgroundPosition = x;
-    }
+document.addEventListener("mousemove", parallax);
+function parallax(e){
+  this.queriSelectorAll('.layer').forEach(layer => {
+    const speed = layer.getAttribute('data-speed')
 
-})();
+    const x = (window.innerWidth - e.pageX*speed)
+    const y = (window.innerHeight - e.pageY*speed)
+
+    layer.style.transform = `translateX(${x}px) translateY(${y}px)`
+  })
+}
