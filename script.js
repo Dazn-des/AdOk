60d5d95ad7a851793fdf16086dd90564825f8ca8
GiuliaPicasso <giulia.picasso@mail.polimi.it>
2022-06-09 19:22:18 +0200

    9/06

--- script.js
+++ script.js
@@ -1,21 +1,56 @@
-document.addEventListener("mousemove", parallax);
-function parallax(e){
-  this.queriSelectorAll('.layer').forEach(layer => {
-    const speed = layer.getAttribute('data-speed')
+//parallasse
 
-    const x = (window.innerWidth - e.pageX*speed)
-    const y = (window.innerHeight - e.pageY*speed)
+(function() {
+    // Add event listener
+    document.addEventListener("mousemove", parallax);
+    const elem = document.querySelector("#parallax");
+    // Magic happens here
+    function parallax(e) {
+        let _w = window.innerWidth/2;
+        let _h = window.innerHeight/2;
+        let _mouseX = e.clientX;
+        let _mouseY = e.clientY;
+        let _depth1 = `${50 - (_mouseX - _w) * 0.01}% ${50 - (_mouseY - _h) * 0.01}%`;
+        let _depth2 = `${50 - (_mouseX - _w) * 0.02}% ${50 - (_mouseY - _h) * 0.02}%`;
+        let _depth3 = `${50 - (_mouseX - _w) * 0.06}% ${50 - (_mouseY - _h) * 0.06}%`;
+        let _depth4 = `${50 - (_mouseX - _w) * 0.06}% ${50 - (_mouseY - _h) * 0.05}%`;
+        let _depth5 = `${50 - (_mouseX - _w) * 0.00}% ${50 - (_mouseY - _h) * 0.00}%`;
+        let x = `${_depth3}, ${_depth2}, ${_depth1}, ${_depth4}, ${_depth5}`;
+        console.log(x);
+        elem.style.backgroundPosition = x;
+    }
 
-    layer.style.transform = `translateX(${x}px) translateY(${y}px)`
-  })
+})();
+
+
+
+
+
+
+//Bottone che torna in alto
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+scrollFunction();
+};
+
+function scrollFunction() {
+if (
+document.body.scrollTop > 20 ||
+document.documentElement.scrollTop > 20
+) {
+mybutton.style.display = "block";
+} else {
+mybutton.style.display = "none";
+}
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+document.body.scrollTop = 0;
+document.documentElement.scrollTop = 0;
 }
 
-
-function openNav() {
-  document.getElementById("mySidenav").style.width = "100%";
-}
-
-/* Close/hide the sidenav */
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-}
+//Bottone che torna in alto
